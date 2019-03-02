43dc4d411c02c4e53d450824f5db26a6eb681522
Patrick Kim <pkim050@gmail.com>
2019-03-02 09:00:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Song
   extend Memorable
   extend Findable
+  include Paramable
   attr_accessor :name
   attr_reader :artist
 
@@ -20,7 +21,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+  #  name.downcase.gsub(' ', '-')
+  #end
 end
