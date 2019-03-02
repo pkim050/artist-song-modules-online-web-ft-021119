6e837b681b5e4b85473e763d3dba704c94ee5b7b
Patrick Kim <pkim050@gmail.com>
2019-03-02 09:25:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
+  include Findable
   include Paramable
   attr_accessor :name
   attr_reader :songs
